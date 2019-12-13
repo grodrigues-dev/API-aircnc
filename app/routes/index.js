b620b9c96843853bf4e87d929007db644bb45a55
grodrigues-dev <52932384+grodrigues-dev@users.noreply.github.com>
2019-12-13 11:34:15 -0300

    Definindo controller de spots

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,8 +1,11 @@
 const express = require('express'); 
 const session = require('../controllers/SessionController')
+const spot  = require('../controllers/SpotController')
 const routes = express.Router();
 
 routes.post('/sessions', session.store);
+routes.post('/spot', session.store);
+
 
 
 module.exports = routes;
