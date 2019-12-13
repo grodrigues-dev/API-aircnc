4f12c1270f18ed8d8422adf9fd887f901eb257de
grodrigues-dev <52932384+grodrigues-dev@users.noreply.github.com>
2019-12-13 15:38:12 -0300

    Criando lógica das reservass

--- app/routes/index.js
+++ app/routes/index.js
@@ -5,13 +5,14 @@ const multer = require('multer');
 const uploadConfig = require('../configs/upload');
 const routes = express.Router();
 const upload = multer(uploadConfig);
-const dashboard = require('../controllers/DashController')
+const dashboard = require('../controllers/DashController'); 
+const booking = require('../controllers/BookingController');
 
 routes.post('/sessions', session.store);
 routes.post('/spots', upload.single('thumbnail'), spot.store);
 routes.get('/spots', spot.index);
 routes.get('/dashboard', dashboard.show);
-
+routes.post('/spots/:spot_id/bookings', booking.store);
 
 
 module.exports = routes;
