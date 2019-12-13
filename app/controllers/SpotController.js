80718f652f6467c5c65dde30e2a70bf8d26e6af1
grodrigues-dev <52932384+grodrigues-dev@users.noreply.github.com>
2019-12-13 12:08:08 -0300

    Armazenando dados de arquivos de mídia no banco

--- app/controllers/SpotController.js
+++ app/controllers/SpotController.js
@@ -1,8 +1,19 @@
+const Spot = require('../models/Spot')
+
 module.exports = {
     async store(req, res){
-        console.log(req.body, req.file);
-        
-        return res.json({"ok" : true});
+        const {fileName} = req.file; 
+        const { company, techs, price} = req.body;
+        const {user_id} = req.headers;
+
+        const spot = await Spot.create({
+            user_id, 
+            thumbnail: fileName, 
+            company, 
+            techs: techs.split(',').map(tech => tech.trim()),
+            price 
+        })
+        return res.json(spot);
     }
 }
 
