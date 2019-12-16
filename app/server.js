ad9557ffc93257a049d2e2617f7f8fcaea7dbff4
grodrigues-dev <52932384+grodrigues-dev@users.noreply.github.com>
2019-12-16 11:21:14 -0300

    retornando url para renderização das imagens

--- app/server.js
+++ app/server.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require('mongoose'); 
 const routes = require('./routes/index'); 
 const cors =require('cors');
+const path =require('path');
 require('dotenv/config');
 
 mongoose.connect(process.env.MONGOCONNECT, {
@@ -12,7 +13,7 @@ mongoose.connect(process.env.MONGOCONNECT, {
 
 app.use(express.json());
 app.use(cors())
-
+app.use('/files', express.static(path.resolve(__dirname, '..' , 'uploads')))
 app.use(routes); 
 
 app.listen(3001, () => console.log("Servidor no ar"));
